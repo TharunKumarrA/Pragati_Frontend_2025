2791011a1966bc8eda1854cc037824602e6d595b
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-14 22:12:54 +0530

    fix: fix

--- src/app/components/home_page.js
+++ src/app/components/home_page.js
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
     return (
       <div className="flex flex-col items-center justify-center h-screen">
         <h1 className="text-6xl font-bold">Welcome to Pragati 2025</h1>

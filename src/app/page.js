54b3bba8267ee2f6fe94022415af803f557fd749
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-14 22:13:30 +0530

    fix: bug

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,12 @@
 "use client";
-import Home from './components/home_page';
-import HeroSection from './components/animation_page/hero_section';
-import LargeInnovationBanner from './components/animation_page/large_innovation_banner';
-import MediumInnovationBanner from './components/animation_page/medium_innovation_banner';
-import SmallInnovationBanner from './components/animation_page/small_innovation_banner';
-import ChallengePrompt from './components/animation_page/challenge_prompt';
+import HomePage from './components/home_page';
+import AnimationPage from './components/animation_page/animation_page';
 
-export default function AnimationPage() {
+export default function Home() {
   return (
     <main>
-      <Home />
-      <HeroSection />
-      <LargeInnovationBanner />
-      <MediumInnovationBanner />
-      <SmallInnovationBanner />
-      <ChallengePrompt />
+      <HomePage />
+      <AnimationPage />
     </main>
   );
 }

428f9a42ff77b5984ff747010adb307777985e64
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-15 16:38:10 +0530

    fix: hydration error fix

--- src/app/components/animation_page/animation_page.js
+++ src/app/components/animation_page/animation_page.js
@@ -1,9 +1,21 @@
 "use client";
-import HeroSection from './hero_section';
-import LargeInnovationBanner from './large_innovation_banner';
-import MediumInnovationBanner from './medium_innovation_banner';
-import SmallInnovationBanner from './small_innovation_banner';
-import ChallengePrompt from './challenge_prompt';
+
+import dynamic from "next/dynamic";
+import HeroSection from "./hero_section";
+import ChallengePrompt from "./challenge_prompt";
+
+// Dynamically import components with SSR disabled
+const LargeInnovationBanner = dynamic(() => import("./large_innovation_banner"), {
+  ssr: false,
+});
+
+const MediumInnovationBanner = dynamic(() => import("./medium_innovation_banner"), {
+  ssr: false,
+});
+
+const SmallInnovationBanner = dynamic(() => import("./small_innovation_banner"), {
+  ssr: false,
+});
 
 export default function AnimationPage() {
   return (

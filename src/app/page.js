822ee643bf76e3dbd3e002395df7714761b463d8
Tharun Kumarr A <tharunkumarra@gmail.com>
2024-12-16 00:12:39 +0530

    atempt to fix conflict

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,27 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import About from "@/app/components/About";
-import Footer from "@/app/components/footer";
 import HeroLanding from "@/app/components/HeroLanding";
 import Sponsors from "@/app/components/Sponsors";
+import About from "@/app/components/About";
 import OlympicConclave from "@/app/components/11_gods/olympic_conclave.jsx";
+import AnimationPage from "@/app/components/animation_page/animation_page";
+import Footer from "@/app/components/footer";
 import FilmGallery from "./components/gallery";
 
 export default function Home() {
-  const pathname = usePathname(); // Get the current pathname
-  const currentPage = pathname === "/" ? "home" : pathname.replace("/", ""); // Map pathname to a readable page name
+  const pathname = usePathname(); 
+  const currentPage = pathname === "/" ? "home" : pathname.replace("/", ""); 
 
   return (
     <div>
       <HeroLanding />
       <Sponsors />
       <About />
+      <AnimationPage />
       <OlympicConclave />
       <FilmGallery />
-      <Footer current_page={currentPage} />
+      <Footer current_page={currentPage} /> {}
     </div>
   );
 }

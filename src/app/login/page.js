d4bb87b53b9339fb22e7a80093a00a410268b1bd
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2024-12-18 09:12:33 +0530

    Remove unused import useEffect.

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,7 +1,7 @@
 "use client";
 
 import styles from "./styles/login.module.css";
-import React, { useState, useEffect } from "react";
+import React, { useState, } from "react";
 import {
   ToastProvider,
   Toast,

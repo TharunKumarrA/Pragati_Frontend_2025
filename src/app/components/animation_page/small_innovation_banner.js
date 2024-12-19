2b4e2fa4fed7ab9e71927284ea9743a17d16ff4a
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-20 03:02:02 +0530

    refactor: added animation of text in small screens

--- src/app/components/animation_page/small_innovation_banner.js
+++ src/app/components/animation_page/small_innovation_banner.js
@@ -1,10 +1,41 @@
 "use client";
 
+import { useEffect, useRef } from "react";
 import Image from "next/image";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function SmallInnovationBanner() {
+  const bannerRef = useRef(null);
+
+  useEffect(() => {
+    const banner = bannerRef.current;
+
+    gsap.fromTo(
+      banner.querySelectorAll("p, h1"),
+      { opacity: 0, y: 50 }, 
+      {
+        opacity: 1,
+        y: 0, 
+        duration: 1.3,
+        stagger: 0.3, 
+        scrollTrigger: {
+          trigger: banner,
+          start: "top 80%", 
+          end: "bottom 20%", 
+          toggleActions: "play reverse play reverse", 
+        },
+      }
+    );
+  }, []);
+
   return (
-    <section className="relative w-full h-[60vh] sm:h-[70vh] flex items-center justify-center overflow-hidden px-4 mx-auto md:hidden">
+    <section
+      ref={bannerRef}
+      className="relative w-full h-[60vh] sm:h-[70vh] flex items-center justify-center overflow-hidden px-4 mx-auto md:hidden"
+    >
       {/* Text Content */}
       <div className="absolute text-center z-10">
         <p className="text-[8vw] [font-family:var(--font-poppins)] leading-tight">Where</p>

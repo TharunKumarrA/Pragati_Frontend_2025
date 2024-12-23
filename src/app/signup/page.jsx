f5e15e4171d3cf3a7d903639cd30c30a05c0f692
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-23 23:54:20 +0530

    refactor: edited page.jsx with updated paths

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Signup from './components/SignUp.jsx';
-import { Toaster } from '@/app/toast/toaster';
-import { ToastProvider } from '@/app/toast/toast';
+import { Toaster } from '@/app/_toast/toaster';
+import { ToastProvider } from '@/app/_toast/toast';
 
 const App = () => {
   return (

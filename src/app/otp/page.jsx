bfb41ea25381b56b690eadc3613844e97959832b
Keerthivasan (Keerthivasan S V) Venkitajalam <cb.sc.u4aie23037@cb.students.amrita.edu>
2024-12-23 23:47:43 +0530

    refactor(edited page.jsx with updated paths): edited page.jsx with updated paths

--- src/app/otp/page.jsx
+++ src/app/otp/page.jsx
@@ -1,6 +1,6 @@
 import Otp from "./component/otp";
-import { Toaster } from '@/app/toast/toaster';
-import { ToastProvider } from '@/app/toast/toast';
+import { Toaster } from '@/app/_toast/toaster';
+import { ToastProvider } from '@/app/_toast/toast';
 
 export default function Page() {
 return (

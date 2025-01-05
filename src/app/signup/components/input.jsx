dc68036bc5f7640ccae2a9b9ac0d71599e960d42
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-01-06 02:21:16 +0530

    perf(signup): cleaned up the signup page code
    Removed unwanted component files and css modules, refactored code to be compact

--- src/app/signup/components/input.jsx
+++ src/app/signup/components/input.jsx
@@ -1,20 +1,25 @@
-import {forwardRef} from "react";
-
+{
+  /*Input Component imported from shadcn */
+}
+import React from "react";
 import { cn } from "@/app/_utils/utils";
 
-const Input = forwardRef(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef((props, ref) => {
+  const { className, type, ...otherProps } = props;
+
   return (
     <input
       type={type}
       className={cn(
-        "flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
         className
       )}
       ref={ref}
-      {...props}
+      {...otherProps}
     />
   );
 });
+
 Input.displayName = "Input";
 
 export { Input };

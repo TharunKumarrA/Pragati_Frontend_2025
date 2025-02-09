39f49feea9cc47f5cc5d7ef862936fc20650dbe8
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-02-09 12:44:34 +0530

    added proper content and pragati 24 recap

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -35,7 +35,6 @@ const About = () => {
           end: "top 50%",
           animation: animation_1,
           scrub: 1.4,
-          markers: false,
         });
       });
     });
@@ -49,7 +48,6 @@ const About = () => {
       });
       details.forEach((detail, index) => {
         let headline = detail.querySelector("h1");
-        //console.log(headline);
         let animation_2 = gsap.timeline();
         animation_2
           .to(photos[index], { yPercent: 0, ease: "power2.inOut" })
@@ -71,8 +69,12 @@ const About = () => {
 
   const pragati =
     "Pragati is the National-level B-Fest hosted by Amrita School of Business every year. The B-Fest comprises of management games crafted for each domain of business administration including Marketing, Finance, Operations, HR and Systems. And year-on-year Pragati has proven its mettle by attracting talents from many leading management institutes in the country, to challenge each other across all domains and aim at achieving the overall championship award.";
+
   const asb =
-    "Amrita School of Business at Coimbatore Campus provides broad-based and specialized business education, preparing individuals to confidently face the ever-growing challenges in the business environment. During their two-year study, students secure essential business knowledge and skills and a deep appreciation for values in business, very critical for effective business performance. School of Business (ASB) has been accredited with AACSB ranking (Association to Advance Collegiate Schools of Business) Internationally in 2019. ASB is the 11th B-School in India to be accredited by AACSB and as of 2022 only 21 Institutions in India has been accredited by AACSB.";
+    "Nestled at the foothills of the Western Ghats, Amrita School of Business, Coimbatore, is more than an institution—it’s a canvas where dreams are painted and leaders are born. Established in 1996 by Mata Amritanandamayi Devi, ASB blends cutting-edge academics with timeless values, inspiring students to lead not just with intellect but with heart. It is a space where challenges become opportunities, innovation thrives, and careers are built with resilience, character, and vision. Ranked among India’s top 27 B-schools, ASB is proudly AACSB-accredited and holds NAAC’s prestigious A++ grade for academic rigor and impact.\n\nAt ASB, education is a transformative journey that unlocks true potential. With dynamic MBA and PhD programs designed to shape the leaders of tomorrow, it goes beyond knowledge to instill excellence with purpose. Every moment here is a step toward brilliance, where passion meets innovation and the future of business leadership takes flight. Welcome to the heart and soul behind Pragati—where ordinary ends, and extraordinary begins!";
+
+  const pragati24 =
+    "Pragati 2024, National Level B Fest concludes The Amrita School of Business, Coimbatore, celebrated another successful edition of Pragati in February 2024, under the theme ‘Multiple Universes, One Extravaganza’. The fest attracted a wide array of participants from numerous business schools, engaging them in a series of management and non-management challenges aimed at sharpening their business skills. A highlight of the event was an inspiring address by Swami Amritaswarupananda Puri, the President of Amrita University, who shared valuable insights and encouragement with the students, further enriching the experience of Pragati 2024. This fest continues to be a pivotal event for fostering innovation, networking, and collaboration among the future luminaries of the business world.";
 
   return (
     <div className="spacer">
@@ -84,20 +86,31 @@ const About = () => {
               <h1 className="font-chicavenue text-[2rem] md:text-[3rem]">
                 About Pragati
               </h1>
-              <p className="text-md md:text-lg text-justify">{pragati}</p>
+              <p className="text-md md:text-lg text-justify">
+                {pragati}
+              </p>
             </div>
             <div className="desktopContentSection flex flex-col justify-center min-h-screen">
               <h1 className="text-[2rem] md:text-[3rem] font-bold">
                 About ASB
               </h1>
-              <p className="text-md md:text-lg text-justify">{asb}</p>
+              <p className="text-md md:text-lg text-justify">
+                {asb}
+              </p>
+            </div>
+            <div className="desktopContentSection flex flex-col justify-center min-h-[50vh] md:min-h-screen">
+              <h1 className="font-chicavenue text-[2rem] md:text-[3rem]">
+                Pragati &apos;24 recap
+              </h1>
+              <p className="text-md md:text-lg text-justify">
+                {pragati24}
+              </p>
             </div>
           </div>
         </div>
 
         {/* Right Section */}
         <div className="right h-[45vh] md:h-screen w-full md:w-1/2 flex flex-col justify-center">
-          {/* Top Image */}
           <div className="topImage w-full h-[5vh] md:h-[10vh] relative overflow-hidden shadow-lg">
             <Image
               src="/Assets/border_style_3.jpg"
@@ -108,7 +121,6 @@ const About = () => {
             />
           </div>
 
-          {/* Main Images */}
           <div className="desktopPhotos h-[40vh] md:h-[80vh] relative overflow-hidden shadow-lg">
             <div className="desktopPhoto absolute w-full h-full">
               <Image
@@ -128,9 +140,17 @@ const About = () => {
                 className="w-full h-full object-cover"
               />
             </div>
+            <div className="desktopPhoto absolute w-full h-full">
+              <Image
+                src="/Images/pragati.webp"
+                width={700}
+                height={700}
+                alt="Pragati 24"
+                className="w-full h-full object-cover"
+              />
+            </div>
           </div>
 
-          {/* Bottom Image */}
           <div className="bottomImage w-full h-[5vh] md:h-[10vh] relative overflow-hidden shadow-lg">
             <Image
               src="/Assets/border_style_3.jpg"

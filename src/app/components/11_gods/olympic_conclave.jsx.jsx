a57775dc004245521696baf9dcd39175d3c3d4a0
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-02-09 20:45:36 +0530

    minor addition

--- src/app/components/11_gods/olympic_conclave.jsx.jsx
+++ src/app/components/11_gods/olympic_conclave.jsx.jsx
@@ -1,7 +1,11 @@
 "use client";
+import React from "react";
 import GodsGallery from "@/app/components/11_gods/gods_gallery";
+import { useRouter } from "next/navigation";
 
 function OlympicConclave() {
+  const router = useRouter();
+
   return (
     <div className="flex flex-col items-center px-4 md:px-16 pt-8 pb-8 bg-[linear-gradient(180deg,#FFF 0%,#E8D096 100%)] w-full h-screen overflow-hidden">
       {/* Main content area */}
@@ -24,7 +28,12 @@ function OlympicConclave() {
 
       {/* Events Button */}
       <button
-        className="mt-4 px-8 md:px-12 py-2 md:py-3 text-xl md:text-3xl tracking-wider text-center text-black border-2 border-black bg-[linear-gradient(90deg,#FACC15,#E8D096)] rounded-full hover:bg-opacity-80 [font-family:var(--font-chicavenue)]"
+        onClick={() => router.push("/upcoming")}
+        className="mt-4 px-8 md:px-12 py-2 md:py-3 text-xl md:text-3xl tracking-wider text-center text-black 
+             border-2 border-black bg-[linear-gradient(90deg,#FACC15,#E8D096)] rounded-full 
+             transition-all duration-300 transform 
+             hover:scale-105 hover:-translate-y-1 hover:shadow-lg hover:bg-opacity-80 
+             active:scale-95 active:translate-y-0 focus:outline-none [font-family:var(--font-chicavenue)]"
         tabIndex={0}
       >
         EVENTS

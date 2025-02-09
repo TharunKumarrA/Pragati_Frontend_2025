857f7d624dce4ed003810363fb9dc016fac93f89
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-02-09 13:31:53 +0530

    navbar color temp fix

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -8,14 +8,14 @@ const Navbar = () => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
-    setMenuOpen((prev) => !prev);  
+    setMenuOpen((prev) => !prev);
   };
 
   return (
     <div>
-      <div className="fixed top-0 left-0 w-full z-10 bg-white/5 backdrop-blur-sm border-white py-1 px-10 flex items-center">
+      <div className="fixed top-0 left-0 w-full z-10 bg-black bg-opacity-20 backdrop-blur-sm border-white py-1 px-10 flex items-center">
         <button className="flex-shrink-0" onClick={toggleMenu}>
-            <Menu className="w-8 h-8 text-white" /> 
+          <Menu className="w-8 h-8 text-white" />
         </button>
         <div className="flex-grow flex justify-center items-center gap-3 md:mr-[-6rem]">
           <Image
@@ -28,12 +28,12 @@ const Navbar = () => {
           <h1 className="text-[2rem] text-white">PRAGATI</h1>
         </div>
         <Link href="/login">
-        <div className="hidden md:block text-[#8B6F47] bg-white px-6 py-2 rounded-full border-[3px] border-[#8B6F47] shadow-md transition-all duration-300 ease-in-out transform hover:bg-[#8B6F47] hover:text-white hover:shadow-lg hover:scale-105 hover:border-[#C2A27E] active:scale-95">
-          <span className="flex items-center gap-2">
-          <User className="w-5 h-5" /> 
-            Login/Sign Up
-          </span>
-        </div>
+          <div className="hidden md:block text-[#8B6F47] bg-white px-6 py-2 rounded-full border-[3px] border-[#8B6F47] shadow-md transition-all duration-300 ease-in-out transform hover:bg-[#8B6F47] hover:text-white hover:shadow-lg hover:scale-105 hover:border-[#C2A27E] active:scale-95">
+            <span className="flex items-center gap-2">
+              <User className="w-5 h-5" />
+              Login/Sign Up
+            </span>
+          </div>
         </Link>
       </div>
       <FullPageMenu isOpen={menuOpen} onClose={toggleMenu} />

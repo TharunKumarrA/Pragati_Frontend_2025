4a1bf1839fb52211f197f093b5d797a9e6f24dde
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-02-12 00:40:56 +0530

    added redirection in nav bar

--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -17,16 +17,18 @@ const Navbar = () => {
         <button className="flex-shrink-0" onClick={toggleMenu}>
           <Menu className="w-8 h-8 text-white" />
         </button>
-        <div className="flex-grow flex justify-center items-center gap-3 md:mr-[-6rem]">
-          <Image
-            src="/Assets/logo.png"
-            width={100}
-            height={100}
-            alt="logo"
-            className="w-12 h-12"
-          />
-          <h1 className="text-[2rem] text-white">PRAGATI</h1>
-        </div>
+        <Link href="/" className="flex-grow">
+          <div className="flex justify-center items-center gap-3 md:mr-[-6rem] cursor-pointer">
+            <Image
+              src="/Assets/logo.png"
+              width={100}
+              height={100}
+              alt="logo"
+              className="w-12 h-12"
+            />
+            <h1 className="text-[2rem] text-white">PRAGATI</h1>
+          </div>
+        </Link>
         <Link href="/login">
           <div className="hidden md:block text-[#8B6F47] bg-white px-6 py-2 rounded-full border-[3px] border-[#8B6F47] shadow-md transition-all duration-300 ease-in-out transform hover:bg-[#8B6F47] hover:text-white hover:shadow-lg hover:scale-105 hover:border-[#C2A27E] active:scale-95">
             <span className="flex items-center gap-2">

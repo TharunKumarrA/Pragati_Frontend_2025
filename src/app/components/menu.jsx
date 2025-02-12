7d1bca1f778c31cccdcccdb5b9ba5336ee67b0ac
Ashrockzzz2003 <ashrockzzz2003@gmail.com>
2025-02-12 20:53:16 +0530

    Improve UI of hero, stats and login button.

--- src/app/components/menu.jsx
+++ src/app/components/menu.jsx
@@ -49,20 +49,20 @@ const FullPageMenu = ({ isOpen, onClose }) => {
           <X className="w-8 h-8 text-white" />
         </button>
 
-        <div className="flex-grow flex justify-center items-center gap-3 md:mr-[-6rem]">
-          <Image
-            src="/Assets/logo.png"
-            width={100}
-            height={100}
-            alt="logo"
-            className="w-12 h-12"
-          />
-          <h1 className="text-[2rem] text-white">PRAGATI</h1>
-        </div>
-        <Link href="/upcoming">
-          {" "}
-          {/* Change this to /login & put the login logic for profile */}
-          <div className="hidden md:block text-[#8B6F47] bg-white px-6 py-2 rounded-full border-[3px] border-[#8B6F47] shadow-md transition-all duration-300 ease-in-out transform hover:bg-[#8B6F47] hover:text-white hover:shadow-lg hover:scale-105 hover:border-[#C2A27E] active:scale-95">
+        <Link href="/">
+          <div className="flex-grow flex justify-center items-center gap-3 md:mr-[-6rem]">
+            <Image
+              src="/Assets/logo.png"
+              width={100}
+              height={100}
+              alt="logo"
+              className="w-12 h-12"
+            />
+            <h1 className="text-[2rem] text-white">PRAGATI</h1>
+          </div>
+        </Link>
+        <Link href="/upcoming"> {/* Change this to /login */}
+          <div className="hidden md:block text-[#1a1a1a] px-6 py-2 rounded-full border-2 border-[#5b4711] bg-opacity-72 bg-[#f9efd4] transition-all duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 active:scale-95">
             <span className="flex items-center gap-2">
               <User className="w-5 h-5" />
               Login/Sign Up
--- src/app/components/navbar.jsx
+++ src/app/components/navbar.jsx
@@ -105,7 +105,7 @@ const Navbar = () => {
           </div>
         ) : (
           <Link href="/upcoming"> {/* Change this to /login */}
-            <div className="hidden md:block text-[#8B6F47] bg-white px-6 py-2 rounded-full border-[3px] border-[#8B6F47] shadow-md transition-all duration-300 ease-in-out transform hover:bg-[#8B6F47] hover:text-white hover:shadow-lg hover:scale-105 hover:border-[#C2A27E] active:scale-95">
+            <div className="hidden md:block text-[#1a1a1a] px-6 py-2 rounded-full border-2 border-[#5b4711] bg-opacity-72 bg-[#fde6a8] transition-all duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 active:scale-95">
               <span className="flex items-center gap-2">
                 <User className="w-5 h-5" />
                 Login/Sign Up

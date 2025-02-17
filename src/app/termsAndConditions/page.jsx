5a6e012a7fcb2b6cf8403accec7a36de95b99afe
Tharun Kumarr A <tharunkumarra@gmail.com>
2025-02-17 18:59:20 +0530

    bug fix

--- src/app/termsAndConditions/page.jsx
+++ src/app/termsAndConditions/page.jsx
@@ -13,12 +13,6 @@ const PrivacyPolicy = () => {
       <div className="max-w-3xl mx-auto">
         <TermsAndConditionsContent />
         <div className="flex justify-center mt-8">
-          <Link
-            href="/"
-            className="text-[#1a1a1a] px-6 py-2 rounded-full border-2 border-[#5b4711] bg-opacity-72 bg-[#fde6a8] transition-all duration-300 ease-in-out transform hover:shadow-lg hover:scale-105 active:scale-95 items-center justify-center "
-          >
-            Go back to home
-          </Link>
         </div>
       </div>
     </div>
